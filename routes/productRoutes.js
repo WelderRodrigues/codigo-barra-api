860dac9b7cd97fc181a8ac51f26b98fdd887c25c
Welder Rodrigues <welder_15@hotmail.com>
2022-08-10 21:16:00 -0300

    Adicionado rota de listagem e busca por id

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -6,6 +6,7 @@ router.post("/", async (req, res) => {
 
   if (!codeBar) {
     res.status(422).json({ error: "Código de barras obrigatório!" });
+    return;
   }
 
   const product = {
@@ -15,7 +16,7 @@ router.post("/", async (req, res) => {
     height,
     depth,
   };
-  //create
+  //Create criando dados
 
   try {
     await Product.create(product);
@@ -23,6 +24,37 @@ router.post("/", async (req, res) => {
     res.status(201).json({ message: "Produto cadastrado com sucesso" });
   } catch (error) {
     res.status(500).json({ error: error });
+  }
+});
+
+//Read - leitura dos dados
+
+//Busca todos os dados
+
+router.get("/", async (req, res) => {
+  try {
+    const allProduct = await Product.find();
+    res.status(200).json(allProduct);
+  } catch (error) {
+    res.status(500).json({ error });
+  }
+});
+
+// Buscar um usuário
+router.get("/:id", async (req, res) => {
+  const id = req.params.id;
+
+  try {
+    const product = await Product.findOne({ _id: id });
+
+    if (!product) {
+      res.status(422).json({ message: "Produto não encontrado." });
+      return;
+    }
+
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ error });
   }
 });
 
